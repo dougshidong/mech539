2570d7148edfa099fa7e7d1ed7f70019ed4abdc2
dougshidong <doug.shidong@gmail.com>
2016-02-05 21:14:54 -0500

    Done plots

--- a1/a1.py
+++ a1/a1.py
@@ -62,6 +62,7 @@ nt = int(np.ceil(tf / dt))
 c = 0.5;
 
 q = 3
+q3calc = 0
 # Exact Solution
 if q == 1 or q == 2:
     xe = np.linspace(0.0, L, 99500, dtype = float)
@@ -69,58 +70,65 @@ if q == 1 or q == 2:
     plt.figure(figsize=(10,5))
     plt.plot(xe, uexact, color = 'k', label = 'Exact')
 
-CFL = 0.5
+CFL = 0.8
 
 if q == 1:
     nscheme = 5
     nxlist = [41]
+    scheme = {0 : upwind,
+              1 : lax,
+              2 : laxwendroff,
+              3 : leapfrog,
+              4 : maccormack}
 if q == 2:
     nscheme = 2
     nxlist = [41, 81, 161, 321]
+    scheme = {0 : upwind,
+              1 : maccormack}
 if q == 3:
     nscheme = 5
-    nxlist = [41, 81, 161, 321, 641, 1281, 2561]
+    nxlist = [41, 81, 161, 321, 641, 1281, 2561, 5121, 10241, 20481, 40561]
     err = np.empty((nscheme, len(nxlist)))
+    scheme = {0 : upwind,
+              1 : lax,
+              2 : laxwendroff,
+              3 : leapfrog,
+              4 : maccormack}
+if q == 4:
+    nscheme = 1
+    nxlist = [41, 81, 161, 162, 167]
+    scheme = {0 : laxwendroff,
+              1 : laxwendroff}
 
 for (nxi, nx) in enumerate(nxlist):
     dx = L / (nx - 1)
     dt = CFL * dx / c
+    if(q == 4):
+        dt = 0.5
+        CFL = c * dt / dx
+        print 'CFL: ', CFL
     nt = int(np.ceil(tf / dt))
 
     usol = np.empty((nscheme, nx))
     x = np.linspace(0.0, L, nx, dtype = float)
     # Initial Condition
     u0 = initCond(x)
-    # Scheme
-    # 0 = Upwind
-    # 1 = Lax
-    # 2 = Lax-Wendroff
-    # 3 = Leap-Frog
-    # 4 = MacCormack
-    if q == 1 or q == 3:
-        scheme = {0 : upwind,
-                  1 : lax,
-                  2 : laxwendroff,
-                  3 : leapfrog,
-                  4 : maccormack}
-    if q == 2:
-        scheme = {0 : upwind,
-                  1 : maccormack}
     
-    # Numerical Solutions
-    for s in range(0, nscheme):
-        if s != 3:
-            u = list(u0)
-            for t in range(nt):
-                u = scheme[s](u, dt, dx)
-        else:
-            u = list(u0)
-            u_old = list(u0)
-            for t in range(nt):
-                u_new = scheme[s](u, u_old, dt, dx)
-                u_old = u
-                u = u_new
-        usol[s, :] = u
+    if(q != 3 or q3calc == 1):
+        # Numerical Solutions
+        for s in range(0, nscheme):
+            if s != 3:
+                u = list(u0)
+                for t in range(nt):
+                    u = scheme[s](u, dt, dx)
+            else:
+                u_old = list(u0)
+                u = scheme[0](u_old, dt, dx)
+                for t in range(1, nt):
+                    u_new = scheme[s](u, u_old, dt, dx)
+                    u_old = u
+                    u = u_new
+            usol[s, :] = u
     
     # Plotting
     plt.rc('text', usetex=True)
@@ -153,28 +161,40 @@ for (nxi, nx) in enumerate(nxlist):
         plt.xlabel(r'$x$')
         plt.ylabel(r'$u$')
     if q == 3:
-        uexact = exactSol(x, c, tf)
-        print err.shape
-        for s in range(0, nscheme):
-            print nxi, s
-            print usol.shape
-            print err.shape
+        if q3calc == 1:
+            print nxlist[nxi]
+            uexact = exactSol(x, c, tf)
+            for s in range(0, nscheme):
+                err[s, nxi] = solError(usol[s, :], uexact)
+    if q == 4:
+        sname = (['Lax-Wendroff',''])
+        for s in range(nscheme):
+            plt.plot(x, usol[s, :], color = col[nxi],
+                     marker = symb[s], mec = col[nxi], mfc = 'None', ms = 6,
+                     label = sname[s] + ' CFL = ' + str(CFL))
+        plt.axis([0, 40, -0.4, 1.1])
+        plt.title(r'Stability Condition')
+        plt.legend(loc='upper left', fontsize = 10)
+        plt.xlabel(r'$x$')
+        plt.ylabel(r'$u$')
 
-            err[s, nxi] = solError(usol[s, :], uexact)
-        print err
 if q == 3:
+    if q3calc == 1:
+        np.save('q3resultscfl08', err)
+    else:
+        err = np.load('q3resultscfl08.npy')
     sname = ('Upwind', 'Lax', 'Lax-Wendroff', 'Leap-Frog', 'MacCormack')
     for s in range(nscheme):
-        plt.semilogy(nxlist, err[s, :], color = col[s],
+        plt.loglog(nxlist, err[s, :], color = col[s],
                  marker = symb[s], mec = col[s], mfc = 'None', ms = 6,
                  label = sname[s])
-    plt.axis([40, 2600, -0.1, 0.2])
+    plt.axis([0, 42000, 0, 0.2])
     plt.title(r'Order of Accuracy')
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper right')
     plt.xlabel(r'Number of Points')
     plt.ylabel(r'Error')
 
 plt.savefig('plot.pdf')
-#plt.savefig('./report/Figures/q3.pdf')
+plt.savefig('./report/Figures/q3.pdf')
 
 # Plotting Q2
